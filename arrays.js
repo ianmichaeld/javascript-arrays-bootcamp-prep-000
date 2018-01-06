0b505981b32a28d7f2459c2dbb9331eca29d3379
Ian Dewsbury <ianmichaeld@gmail.com>
2018-01-06 18:09:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(a, e){
-  var arr = a;
+  var arr = [...a];
   arr.unshift(e);
   return arr;
 }
